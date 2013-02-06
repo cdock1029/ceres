0ae655df4574bf5b53fd69cf47e37de6612d163f
Conor Dockry <conordockry@gmail.com>
2013-02-06 15:56:09 -0500

    insert.js edits

--- src/insert.js
+++ src/insert.js
@@ -1,19 +1,25 @@
 var MongoClient = require('mongodb').MongoClient;
-var responseHandler = require('./responseHandlers');
+var responseHandlers = require('./responseHandlers');
 var schemaValidation = require('./schemaValidation');
 
 function insert(data, timestamp, response) {
 
 	schemaValidation.validate(data, function(err) {
 		if(err) {
-			//do something if not valid.	
+			console.log(err);
+			responseHandlers.invalidRequest(response, 2);
 		} else {
 			MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-				if(err) { return console.dir(err); }
+				if(err) { 
+					console.log(err);
+					responseHandlers.invalidRequest(response, 2);
+				}
 				db.collection('test', function(err, collection) {
 					collection.insert(data, {w:1}, function(err, result) {
 						if(err) {
 							//do something with db error
+						} else {
+							responseHandlers.validRequest(response, false, result);
 						}
 					});
 				});
