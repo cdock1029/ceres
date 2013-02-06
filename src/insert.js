7eb4ccf47c3a93b008aca99f083df4f280c9a54b
Conor Dockry <conordockry@gmail.com>
2013-02-06 17:06:31 -0500

    handle err objects with responseHandler

--- src/insert.js
+++ src/insert.js
@@ -3,26 +3,35 @@ var responseHandlers = require('./responseHandlers');
 var schemaValidation = require('./schemaValidation');
 
 function insert(data, timestamp, response) {
-
 	schemaValidation.validate(data, function(err) {
 		if(err) {
 			console.log(err);
 			responseHandlers.invalidRequest(response, 2);
 		} else {
+			//hard coded database address and name. Needs refactored.
 			MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
 				if(err) { 
 					console.log(err);
 					responseHandlers.invalidRequest(response, 2);
-				}
-				db.collection('test', function(err, collection) {
-					collection.insert(data, {w:1}, function(err, result) {
+				} else {
+					//hard coded collection name. Needs refactored.
+					db.collection('test', function(err, collection) {
 						if(err) {
-							//do something with db error
+							console.log(err);
+							responseHandlers.invalidRequest(response, 2);
 						} else {
-							responseHandlers.validRequest(response, false, result);
+							collection.insert(data, {w:1}, function(err, result) {
+								if(err) {
+									//do something with db error
+									console.log(err);
+									responseHandlers.invalidRequest(response, 2);
+								} else {
+									responseHandlers.validRequest(response, false, result);
+								}
+							});
 						}
 					});
-				});
+				}
 			});		
 		}
 	});
