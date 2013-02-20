7be018f354a7407616a32c109e7ee9ab95efc361
Aneeth Krishnamoorthy <krishnamoorthy.15@osu.edu>
2013-02-20 22:06:42 +0000

    Config changes to delete and deleteAll

--- src/deleteAll.js
+++ src/deleteAll.js
@@ -1,40 +1,33 @@
-
-var MongoClient = require('mongodb').MongoClient;
 var responseHandlers = require('./responseHandlers');
 
 function deleteAll(timestamp, response) {
-	//hard coded database address and name. Needs refactored.
-	MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-	if(err) {
-		console.log(err);
-		responseHandlers.invalidRequest(response, 2);
-	} else {
-		//hard coded collection name. Needs refactored.
-		db.collection('test', function(err, collection) {
+    var mongoDb = require('mongodb');
+    var server = new mongoDb.Server(mongoConfig.host,mongoConfig.port,{'auto_reconnect': true});
+    var db = new mongoDb.Db(mongoConfig.database, server, {w: 1});
+    db.open(function(err, db) {
 		if(err) {
 			console.log(err);
 			responseHandlers.invalidRequest(response, 2);
 		} else {
-			/*
-			To remove all documents in a collection, call the remove method with no parameters:
-				db.collection.remove()
-			*/
-				collection.remove({}, function(err, result) {
+			db.collection(mongoConfig.collection, function(err, collection) {
 				if(err) {
-					//do something with db error
-					console.log("Error: Database may be empty or you don't have permisson!");
 					console.log(err);
 					responseHandlers.invalidRequest(response, 2);
 				} else {
-					responseHandlers.validRequest(response, true, result);
+					collection.remove({}, function(err, result) {
+						if(err) {
+							console.log(err);
+							responseHandlers.invalidRequest(response, 2);
+						} else {
+							console.log('Delete All successful');
+							responseHandlers.validRequest(response, true, result);
+						}
+						db.close();
+					});
 				}
+			
 			});
-			}
-		});
-	}
+		}
 	});	
 }
-
-
-
 exports.deleteAll = deleteAll;
