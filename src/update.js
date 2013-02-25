147189f14dd496565f027ce11e20baece3a706a5
Aneeth Krishnamoorthy <krishnamoorthy.15@osu.edu>
2013-02-25 21:50:45 +0000

    Change to add database set-up info.

--- src/update.js
+++ src/update.js
@@ -7,6 +7,9 @@ function update(expression, data, timestamp, response) {
 			console.log(err);
 			responseHandlers.invalidRequest(response, 2);
 		} else {
+            var mongoDb = require('mongodb');
+            var server = new mongoDb.Server(mongoConfig.host,mongoConfig.port,{'auto_reconnect': true});
+            var db = new mongoDb.Db(mongoConfig.database, server, {w: 1});			
 			db.open(function(err, db) {
 				if(err) { 
 					console.log(err);
