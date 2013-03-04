114653410bab4f52fe9d64f637ac32290a22ad27
Dave Hewitt <davidshewitt@gmail.com>
2013-03-04 18:05:24 -0500

    stressTest now runs multiple clients

--- test/stressTest.sh
+++ test/stressTest.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#the number of simultaneous clients
+NUM_CLIENTS=10;
+#the number of records for each client to insert
+NUM_RECORDS=100;
 echo "Before"
 date
-for i in {1..100}
+for i in {1..$NUM_CLIENTS }
 do
-	curl -d @collect1.json http://localhost:8888/collect
+	 node testrunner.js POST localhost 8888 /collect collect1.json $NUM_RECORDS &
 done
 echo "After"
 date
