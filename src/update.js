5b4d366a05230c474644a52b158359de0d4628d6
Conor Dockry <conordockry@gmail.com>
2013-04-01 21:11:48 +0000

    mongodb uri change for update.js

--- src/update.js
+++ src/update.js
@@ -9,7 +9,8 @@
 var responseHandlers = require('./responseHandlers');
 var updateValidation = require('./updateValidation');
 var monGo = require('mongodb');
-
+var MongoClient = require('mongodb').MongoClient,
+		ObjectID = require('mongodb').ObjectID;
 function update(obj_id, data, timestamp, response) {
 	//validate object id
 	updateValidation.validate(obj_id, function(err) {
@@ -17,11 +18,8 @@ function update(obj_id, data, timestamp, response) {
 			console.log(err);
 			responseHandlers.invalidRequest(response, 2);
 		} else {
-			// openning the database
-            var mongoDb = require('mongodb');
-            var server = new mongoDb.Server(mongoConfig.host,mongoConfig.port,{'auto_reconnect': true});
-            var db = new mongoDb.Db(mongoConfig.database, server, {w: 1});			
-			db.open(function(err, db) {
+				// openning the database
+				MongoClient.connect(mongoConfig.uri, function(err, db) {
 				if(err) { 
 					console.log(err);
 					responseHandlers.invalidRequest(response, 2);
@@ -33,13 +31,13 @@ function update(obj_id, data, timestamp, response) {
 						} else {
 							// Querying the db for the record with that object id, updating it data, and re-inserting it back.
 							collection.update({"_id": new monGo.ObjectID(obj_id)}, {$set: data} , function(err, result) {
+								db.close();	
 								if(err) {
 									console.log(err);
 									responseHandlers.invalidRequest(response, 2);
 								} else {
 									responseHandlers.validRequest(response, true, result);
 								}
-								db.close();	
 							});
 						}
 					});
