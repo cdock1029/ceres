7695b50ac5fb52ecbfd4c0c893f627e5caef194a
Conor Dockry <conordockry@gmail.com>
2013-04-01 22:05:19 +0000

    mongodb uri changes,response object work

--- src/deleteAll.js
+++ src/deleteAll.js
@@ -7,24 +7,29 @@
 
 
 var responseHandlers = require('./responseHandlers');
-
+var MongoClient = require('mongodb').MongoClient;
 function deleteAll(response) {
 	//openning the database
-    var mongoDb = require('mongodb');
-    var server = new mongoDb.Server(mongoConfig.host,mongoConfig.port,{'auto_reconnect': true});
-    var db = new mongoDb.Db(mongoConfig.database, server, {w: 1});
-    db.open(function(err, db) {
+    //var mongoDb = require('mongodb');
+    //var server = new mongoDb.Server(mongoConfig.host,mongoConfig.port,{'auto_reconnect': true});
+    //var db = new mongoDb.Db(mongoConfig.database, server, {w: 1});
+    //db.open(function(err, db) {
+		console.log("Before connect");
+		MongoClient.connect(mongoConfig.uri, function(err, db) {
 		if(err) {
 			console.log(err);
 			responseHandlers.invalidRequest(response, 2);
 		} else {
+			console.log("after sucessful connect call. before collection.")
 			db.collection(mongoConfig.collection, function(err, collection) {
 				if(err) {
 					console.log(err);
 					responseHandlers.invalidRequest(response, 2);
 				} else {
-				//remove all records (that's what the empty query is for)
+					//remove all records (that's what the empty query is for)
+					console.log("after collection, before remove.");
 					collection.remove({}, function(err, result) {
+						db.close();
 						if(err) {
 							console.log(err);
 							responseHandlers.invalidRequest(response, 2);
@@ -32,7 +37,6 @@ function deleteAll(response) {
 							console.log('Delete All successful');
 							responseHandlers.validRequest(response, true, result);
 						}
-						db.close();
 					});
 				}
 			
--- src/insert.js
+++ src/insert.js
@@ -43,7 +43,8 @@ function insert(data, timestamp, response) {
 									responseHandlers.invalidRequest(response, 2);
 								} else {
 									console.log('Insert successful');
-									responseHandlers.validRequest(response, false, result);
+									var retVal = {"_id": result._id};
+									responseHandlers.validRequest(response, false, retVal);
 								}
 							});
 						}
